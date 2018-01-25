2b4566e81fc88e0c9f76ee44a09b7fa778e82e35
aslihanhastaoglu <33522577+aslihanhastaoglu@users.noreply.github.com>
2018-01-25 14:54:56 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 <?php
-include_once "includes/ez_sql_core.php";
-include_once "includes/ez_sql_mysqli.php";
-include_once "includes/config.php";
+include_once "ez_sql_core.php";
+include_once "ez_sql_mysqli.php";
+include_once "config.php";
 $db = new ezSQL_mysqli($db_user, $db_password, $db_name, $db_host);
 $db->query("SET NAMES utf8"); 
 $db->query("DELETE FROM contacts WHERE id='$_POST[id]'");
